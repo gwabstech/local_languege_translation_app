133b3af2d39ff5118042b0e817c2ca803d9f2d6d
Gwabstech <gwabstech@gmail.com>
2023-09-02 15:53:50 +0100

    Working on image

---
+++ app/src/main/java/com/gwabs/englishtohausatexttranslation/imageToText.kt
@@ -0,0 +1,11 @@
+package com.gwabs.englishtohausatexttranslation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class imageToText : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_image_to_text)
+    }
+}
