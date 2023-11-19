545fc10e39f6156508586b9eb2ced953546a1c84
Gwabstech <gwabstech@gmail.com>
2023-11-19 19:03:55 +0100

    Done

--- app/src/main/java/com/gwabs/englishtohausatexttranslation/Reusebles.kt
+++ app/src/main/java/com/gwabs/englishtohausatexttranslation/Reusebles.kt
@@ -8,6 +8,10 @@ fun getLanguageCode(language: String): String {
     return when (language) {
         "English" -> "en"
         "Hausa" -> "ha"
+        "Igbo" -> "ig"
+        "Yoruba" -> "yo"
+        "French" -> "fr"
+        "Arabic" -> "ar"
 
 
         else -> throw IllegalArgumentException("Unsupported language: $language")
